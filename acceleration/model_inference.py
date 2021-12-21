81468bf07a72af0fd7fbf18fde1a27dfdad14862
GB0ZqhA1 <77417203+GB0ZqhA1@users.noreply.github.com>
2021-12-21 21:06:08 +0900

    Update model_inference.py

--- acceleration/model_inference.py
+++ acceleration/model_inference.py
@@ -1,36 +1,19 @@
 import argparse
 import os
-import shutil
 import warnings
-from concurrent.futures import ProcessPoolExecutor
 from gcp import *
-from tqdm import tqdm
-from torch.optim.lr_scheduler import CosineAnnealingLR
+from torchvision.models.resnet import BasicBlock, Bottleneck
 
 import torch
-import torch.nn as nn
 import torch.nn.parallel
-import torch.backends.cudnn as cudnn
-from torch.cuda.amp import GradScaler, autocast
-import torch.distributed as dist
 import torch.optim
 import torch.multiprocessing as mp
 import torch.utils.data
 import torch.utils.data.distributed
-import torchvision.transforms as transforms
-import torchvision.datasets as datasets
 import torchvision.models as models
-from inf_resnet import *
+from inf_model import *
 
 warnings.filterwarnings("ignore")
-
-#########################################
-executionID = "0"
-comp = 0.25
-groups = 4
-rampath = '/media/ramdisk/'
-########################################
-os.makedirs(rampath, exist_ok=True)
 
 model_names = sorted(name for name in models.__dict__
     if name.islower() and not name.startswith("__")
@@ -44,13 +27,30 @@ parser.add_argument('-a', '--arch', metavar='ARCH', default='resnet18',
                         ' (default: resnet18)')
 parser.add_argument('-m', '--model', metavar='MODEL', default=None,
                     help='model file')
-parser.add_argument('-b', '--batch-size', default=64, type=int,
-                    metavar='N',
-                    help='mini-batch size (default: 256), this is the total '
-                         'batch size of all GPUs on the current node when '
-                         'using Data Parallel or Distributed Data Parallel')
 parser.add_argument('-j', '--workers', default=16, type=int, metavar='N',
                     help='number of data loading workers (default: 4)')
+
+def set_groups(network,groups,inds):
+    for m in network.modules():
+        if isinstance(m, BasicBlock):
+            m.conv2.weight.groups = groups[0]
+            m.conv1.weight.groups = groups[1]
+            m.conv2.weight.ind = inds[0]
+            m.conv1.weight.ind = inds[1]
+            if len(groups)>1:
+                groups = groups[2:]
+                inds = inds[2:]
+                
+        elif isinstance(m, Bottleneck):
+            m.conv3.weight.groups = groups[0]
+            m.conv2.weight.groups = groups[1]
+            m.conv1.weight.groups = groups[2]
+            m.conv3.weight.ind = inds[0]
+            m.conv2.weight.ind = inds[1]
+            m.conv1.weight.ind = inds[2]
+            if len(groups)>1:
+                groups = groups[3:]
+                inds = inds[3:]
 
 
 def main():
@@ -58,84 +58,89 @@ def main():
     main_worker(args)
 
 
+loc = 'cuda'
+
 def main_worker(args):
     global comp
     global groups
     loadname = args.model
     
-    model = models.__dict__[args.arch]()
-    model.cuda()
-    for m in model.modules():
-        if isinstance(m, models.resnet.BasicBlock):
-            m.conv1.weight.position=0
-            m.conv2.weight.position=1
-        if isinstance(m, models.resnet.Bottleneck):
-            m.conv1.weight.position=0
-            m.conv2.weight.position=1
-            m.conv3.weight.position=2
-    optimizer = SGD_GCP(model.parameters(), lr=0, momentum=0, weight_decay=0,
-                        ratio=comp, numgroup=4)
-    if os.path.isfile(loadname):
-        print("=> loading checkpoint '{}'".format(loadname))
-        # Map model to be loaded to specified single gpu.
-        loc = 'cuda'
-        checkpoint = torch.load(loadname, map_location=loc)
-        from collections import OrderedDict
-        new_state_dict = OrderedDict()
-        for k, v in checkpoint.items():
-            name = k[15:] # remove `module.`
-            new_state_dict[name] = v
-        # load params
-        model.load_state_dict(new_state_dict)
-        print("=> loaded checkpoint '{}'"
-                .format(loadname))
+    org_model = models.__dict__[args.arch](pretrained=True)
+    org_model.to(loc)
+    if loadname is None:
+        model = org_model
     else:
-        print("=> no checkpoint found at '{}'".format(loadname))
-        return
+        model = models.__dict__[args.arch]()
+        model.to(loc)
+        if os.path.isfile(loadname):
+            print("=> loading checkpoint '{}'".format(loadname))
+            checkpoint = torch.load(loadname, map_location=loc)
+            from collections import OrderedDict
+            new_state_dict = OrderedDict()
+            for k, v in checkpoint['state_dict'].items():
+                name = k[15:] # remove `module.`
+                new_state_dict[name] = v
+            model.load_state_dict(new_state_dict)
+            print("=> loaded checkpoint '{}' (epoch {}, best_acc1 {})"
+                    .format(loadname, checkpoint['epoch'], checkpoint['best_acc1']))
+        else:
+            print("=> no checkpoint found at '{}'".format(loadname))
 
-    optimizer.cluster()
-
+    groups = torch.load(args.arch+'R_8x_0.4_groups.pkl', map_location=torch.device(loc))
+    inds = torch.load(args.arch+'R_8x_0.4_inds.pkl', map_location=torch.device(loc))
+    pruner = GCP(model, 0, 0, 8)
+    set_groups(model, groups, inds)
     if args.arch == "resnet18":
-        inf_model = inf_resnet18(groups=groups, comp=comp).cuda()
+        inf_model = inf_resnet18().to(loc)
     elif args.arch == "resnet50":
-        inf_model = inf_resnet50(groups=groups, comp=comp).cuda()
-    model.eval()
-    inf_model.eval()
+        inf_model = inf_resnet50().to(loc)
 
     for m1, m2 in zip(model.modules(),inf_model.modules()):
         if isinstance(m1, models.resnet.BasicBlock) or isinstance(m1, models.resnet.Bottleneck) or\
             isinstance(m1, models.resnet.ResNet):
             m2.load_from_pruned_checkpoints(m1)
-    inf_model = torch.jit.optimize_for_inference(torch.jit.trace(inf_model, torch.rand(64,3,224,224).cuda()))
     
-    print('Results with acceleration')
-    validate(inf_model, args)
-    print('Results without acceleration (Same speed to the original model)')
-    validate(model, args)
-    
+    model.eval()
+    inf_model.eval()
+
+    args.batch_size=128
+    input_tensor = torch.randn(128,3,224,224).to(loc)
+    inf_model = torch.jit.optimize_for_inference(torch.jit.trace(inf_model, input_tensor))
+    print('Speed of the original model')
+    validate(org_model, args, count=100)
+    print('Speed of the accelerated model')
+    validate(inf_model, args, count=100)
+    #cudabench(inf_model, input_tensor)
+    torch.jit.save(inf_model, 'pruned_'+args.arch+'.pt')
     return
 
-def validate(model, args):
-    batch_time = AverageMeter('Time', ':6.3f')
-    # switch to evaluate mode
+def cudabench(model, input_tensor, count=100):
     with torch.no_grad():
-        for i in range(100):
-            images = torch.randn(64,3,224,224).cuda()
+        with torch.autograd.profiler.profile(use_cuda=True, with_flops=True) as prof:
+            for _ in range(count):
+                output = model(input_tensor)
+        print(prof.key_averages().table(sort_by="self_cuda_time_total"))
 
-            # compute output
+def validate(model, args, count=100000, verbose=True):
+    batch_time = AverageMeter('Time', ':6.3f')    
+    with torch.no_grad():
+        for _ in range(count):
+        
+            images = torch.randn(args.batch_size,3,224,224).to(loc)
+
             start = torch.cuda.Event(enable_timing=True)
             end = torch.cuda.Event(enable_timing=True)
             start.record()
             output = model(images)
             end.record()
-            # measure elapsed time
             torch.cuda.synchronize()
             batch_time.update(start.elapsed_time(end))
-            
-        print(' * Time {batch_time.avg:.3f}'
-              .format(batch_time=batch_time))
+        
+    
+        if verbose:
+            print(' * Time {batch_time.avg:.3f}'.format(batch_time=batch_time))
 
-    return 0
+    return
 
 class AverageMeter(object):
     """Computes and stores the average and current value"""
@@ -159,40 +164,6 @@ class AverageMeter(object):
     def __str__(self):
         fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
         return fmtstr.format(**self.__dict__)
-
-
-class ProgressMeter(object):
-    def __init__(self, num_batches, meters, prefix=""):
-        self.batch_fmtstr = self._get_batch_fmtstr(num_batches)
-        self.meters = meters
-        self.prefix = prefix
-
-    def display(self, batch):
-        entries = [self.prefix + self.batch_fmtstr.format(batch)]
-        entries += [str(meter) for meter in self.meters]
-        print('\t'.join(entries))
-
-    def _get_batch_fmtstr(self, num_batches):
-        num_digits = len(str(num_batches // 1))
-        fmt = '{:' + str(num_digits) + 'd}'
-        return '[' + fmt + '/' + fmt.format(num_batches) + ']'
-
-def accuracy(output, target, topk=(1,)):
-    """Computes the accuracy over the k top predictions for the specified values of k"""
-    with torch.no_grad():
-        maxk = max(topk)
-        batch_size = target.size(0)
-
-        _, pred = output.topk(maxk, 1, True, True)
-        pred = pred.t()
-        correct = pred.eq(target.view(1, -1).expand_as(pred))
-
-        res = []
-        for k in topk:
-            correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
-            res.append(correct_k.mul_(100.0 / batch_size))
-        return res
-
 
 if __name__ == '__main__':
     main()
